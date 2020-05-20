1be320de436f37c98561520e628189782cc8569a
Meziu <55318903+Meziu@users.noreply.github.com>
2020-05-20 21:02:47 +0200

    3rd try, will it work?

--- app.py
+++ app.py
@@ -95,6 +95,15 @@ def search():
         Highscore.time_string,
         Highscore.datetime) \
         .filter(Map.id == Highscore.map_id)
+    
+    order = request.args.get('order')
+    order_direction = request.args.get('order_direction')
+    if order in key_to_column:
+        order_by = key_to_column[order]
+        if order_direction == 'desc':
+            order_by = order_by.desc()
+        query = query.order_by(order_by)
+    query = query.order_by(Highscore.time.asc())
 
     for key in request.args:
         if key in key_to_column:
@@ -105,15 +114,6 @@ def search():
             query = query.limit(int(limit))
         except ValueError:
             pass
-
-    order = request.args.get('order')
-    order_direction = request.args.get('order_direction')
-    if order in key_to_column:
-        order_by = key_to_column[order]
-        if order_direction == 'desc':
-            order_by = order_by.desc()
-        query = query.order_by(order_by)
-    query = query.order_by(Highscore.time.asc())
 
     result = query.all()
     resp = Response(response=to_json(result), status=200, mimetype="application/json")
