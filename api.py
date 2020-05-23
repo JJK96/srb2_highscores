fb0179f5d89cd9ed40b02c5f72a7b142416f0fcf
Meziu <meziu210@icloud.com>
2020-05-23 17:03:38 +0200

    Implemented the best users and skins pages

--- api.py
+++ api.py
@@ -2,44 +2,57 @@ from database import db, Map, Highscore, key_to_column
 from flask import Blueprint, request, Response
 from dataclasses import dataclass, field
 import json
+from collections import defaultdict
 
+# setup the api section of the site
 api_prefix = "/api"
 api_routes = Blueprint('api', __name__)
 
+
+# class for the api search parameters
 @dataclass
 class GetParam:
     param: str
     description: str
     values: list = None
 
+# class for the api endpoints
 @dataclass
 class Endpoint:
     url: str
     description: str
     get_params: list = field(default_factory=list)
 
+
+# convert a list to json
 def to_json(s):
     return json.dumps([x._asdict() for x in s], default=lambda o: str(o))
 
+# get all the users in the database
 def get_users():
     users = db.session.query(Highscore.username).distinct().all()
     return [x.username for x in users]
 
+# get all the skins in the database
 def get_skins():
     skins = db.session.query(Highscore.skin).distinct().all()
     return [x.skin for x in skins]
 
+# get all the maps in the database
 def get_maps():
     query = db.session.query(Map)
     return query.all()
 
-def get_highscores():
+# get the best highscores for each skin in each map
+def get_map_highscores():
+    # get the best time for each map and skin
     best_map = db.session.query(
         db.func.min(Highscore.time).label("time"),
         Highscore.skin,
         Highscore.map_id) \
         .group_by(Highscore.skin,
                   Highscore.map_id).subquery()
+    # get all the highscores and select the scores from the found best scores
     query = db.session.query(
         Map.id.label("map_id"),
         Map.name.label("mapname"),
@@ -55,8 +68,12 @@ def get_highscores():
                           (Highscore.time == best_map.c.time))) \
         .filter(Map.id == Highscore.map_id) \
         .order_by(Map.id, Highscore.time.asc())
+    
     maps = {}
+    
+    # for every score in the filtered scores
     for row in query.all():
+        # save the scores
         map = maps.get(row.map_id, {
             'id': row.map_id,
             'name': row.mapname,
@@ -72,8 +89,109 @@ def get_highscores():
         maps[row.map_id] = map
     return [x for x in maps.values()]
 
+# get all the highscores
+def get_highscores():
+    # get all the highscores from the database ordered by time
+    query = db.session.query(
+        Highscore.username,
+        Map.name.label("mapname"),
+        Map.id.label("map_id"),
+        Highscore.skin,
+        Highscore.time,
+        Highscore.time_string,
+        Highscore.datetime) \
+        .filter(Map.id == Highscore.map_id) \
+        .order_by(Highscore.time.asc())
+        
+    return query.all()
+
+# get all the highscores divided by map
+def get_highscores_by_map():
+    # get all the highscores
+    highscores = get_highscores()
+    # get all the maps
+    maps = get_maps()
+    # setup the results' dictionary
+    res = {}
+    # for every map
+    for map in maps:
+        temp = []
+        # for every score
+        for score in highscores:
+            # if the score's map is the same as the current map
+            if score[1] == map.name:
+                # append the score to the map's list
+                temp.append(score)
+        # append the map's list by the map's name
+        res[map.name] = temp
+    return res
+
+# Gets the best skins or leaderboard of users in the database 
+# returns the skins when arg with_weights is false and the leaderboard when it's true
+def get_best_in_data(with_weights):
+    # setup the dictionaries for the storing of the results
+    res = {}
+    scoring = defaultdict(int)
+    
+    # setup the weights for the leaderboard(mariokart's scoring system)
+    weights = {
+    1:15,
+    2:12,
+    3:10,
+    4:8,
+    5:7,
+    6:6,
+    7:5,
+    8:4,
+    9:3,
+    10:2,
+    11:1,
+    12:0
+    }
+    
+    # get the highscores divided by map
+    highscores = get_highscores_by_map()
+    
+    # for every map in the highscores
+    for map_scores in highscores:
+        # if must return the best skins
+        if not with_weights:
+            # get the scores
+            scores = highscores[map_scores]
+            # save the point in the dictionary by the first score's skin
+            scoring[scores[0][3]] += 1
+        else:
+            # reset counted usernames, offset and place
+            counted_usernames = []
+            offset = 0
+            place = 0
+            # for every score in the map's highscores
+            for score in highscores[map_scores]:
+                # increase the place count
+                place += 1
+                # if 12 scores got counted
+                if place-offset >= 12:
+                    # get the next map
+                    break
+                # if the score's username hasn't been counted yet
+                if not score[0] in counted_usernames:
+                    # add increase the points in the dictionary by the username
+                    scoring[score[0]] += weights.get(place-offset, 0)
+                    # set the username as counted
+                    counted_usernames.append(score[0])
+                else:
+                    # add offset to the score counting
+                    offset += 1
+    # sort the dictionary by most points
+    for k, v in sorted(scoring.items(), key=lambda x: x[1], reverse=True):
+        res[k] = v
+    return res
+
+
+# when the route is api/
 @api_routes.route('/')
 def api():
+    # show the docs for every endpoint in the api section
     endpoints = [
         Endpoint(f'{api_prefix}/maps', 'Return all maps'),
         Endpoint(f'{api_prefix}/search', 'Return highscores ordered by time ascending', [
@@ -88,35 +206,63 @@ def api():
         Endpoint(f'{api_prefix}/highscores', 'Get best scores per map and skin'),
         Endpoint(f'{api_prefix}/skins', 'Get the different skins in the database'),
         Endpoint(f'{api_prefix}/users', 'Get the different users in the database'),
-    ]
+        Endpoint(f'{api_prefix}/leaderboard', 'Get the leaderboard of the best players'),
+        Endpoint(f'{api_prefix}/bestskins', 'Get the best skins by number of best timed tracks'),
+        Endpoint(f'{api_prefix}/maphighscores', 'Get the highscores divided by map')
+        ]
+    # return the docs as json
     response = json.dumps({
         'endpoints': endpoints
     }, default=lambda o: o.__dict__)
     resp = Response(response=response, status=200, mimetype="application/json")
     return resp
 
+# when the route is api/maps
 @api_routes.route('/maps')
 def maps():
+    # return the maps as json
     resp = Response(response=str(get_maps()), status=200, mimetype="application/json")
     return resp
 
+# when the route is api/users
 @api_routes.route('/users')
 def api_users():
+    # return the users as json
     resp = Response(response=json.dumps(get_users()), status=200, mimetype="application/json")
     return resp
 
+# when the route is api/skins
 @api_routes.route('/skins')
 def api_skins():
+    # return the skins as json
     resp = Response(response=json.dumps(get_skins()), status=200, mimetype="application/json")
     return resp
 
-@api_routes.route('/highscores')
-def api_highscores():
-    resp = Response(response=json.dumps(get_highscores(), default=lambda o: str(o)), status=200, mimetype="application/json")
+# when the route is api/leaderboard
+@api_routes.route('/leaderboard')
+def api_leaderboard():
+    # return the leaderboard as json
+    resp = Response(response=json.dumps(get_best_in_data(True)), status=200, mimetype="application/json")
     return resp
 
+# when the route is api/bestskins
+@api_routes.route('/bestskins')
+def api_best_skins():
+    # return the best skins as json
+    resp = Response(response=json.dumps(get_best_in_data(False)), status=200, mimetype="application/json")
+    return resp
+
+# when the route is api/maphighscores
+@api_routes.route('/maphighscores')
+def api_highscores():
+    # return the highscores for each skin in each map as json
+    resp = Response(response=json.dumps(get_map_highscores(), default=lambda o: str(o)), status=200, mimetype="application/json")
+    return resp
+
+# when the route is api/search
 @api_routes.route('/search')
 def search():
+    # get all the highscores NOT ORDERED
     query = db.session.query(
         Highscore.username,
         Map.name.label("mapname"),
@@ -127,25 +273,40 @@ def search():
         Highscore.datetime) \
         .filter(Map.id == Highscore.map_id)
     
+    # request the params for the ordering
     order = request.args.get('order')
     descending = request.args.get('descending')
+    # if the order param is valid
     if order in key_to_column:
+        # order by the order parameter
         order_by = key_to_column[order]
+        # if the descending parameter got passed
         if descending:
+            # order in descending order
             order_by = order_by.desc()
         query = query.order_by(order_by)
     query = query.order_by(Highscore.time.asc())
 
+    # for every given parameter
     for key in request.args:
+        # if the parameter's key is in the highscores columns
         if key in key_to_column:
+            # filter the highscores by such column
             query = query.filter(key_to_column[key] == request.args.get(key))
+    
+    # request the limit parameter
     limit = request.args.get('limit')
+    # if the parameter is given
     if limit:
+        # try to limit the highscores by the limit
         try:
             query = query.limit(int(limit))
+        # if the limit isn't a number
         except ValueError:
+            # don't do anything
             pass
-
-    result = query.all()
-    resp = Response(response=to_json(result), status=200, mimetype="application/json")
+    
+    # return the query as json
+    scores = query.all()
+    resp = Response(response=to_json(scores), status=200, mimetype="application/json")
     return resp
--- app.py
+++ app.py
@@ -3,29 +3,37 @@ from api import *
 from database import db, key_to_column
 from settings import username, password, host, database
 
+# config the site
 class Config:
     static_dir = "static"
     base_url = ""
 
+# setup the class as a Flask object
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{username}:{password}@{host}/{database}"
 app.register_blueprint(api_routes, url_prefix=api_prefix)
 
+# init the database connector
 db.init_app(app)
 
+# when the route is /
 @app.route('/')
 def central_hub():
+    # show the main page
     return render_template('index.html', config=Config)
 
+# when the route is /bestformaps
 @app.route('/bestformaps')
 def highscores_map_skin():
+    # show the best for maps page
     return render_template('best_for_maps.html',
-                           highscores=get_highscores(),
-                           maps=get_maps(),
+                           highscores=get_map_highscores(),
                            config=Config)
 
+# when the route is /search
 @app.route('/search')
 def home():
+    # show the search page
     return render_template('search.html',
                            maps=get_maps(),
                            columns=[x for x in key_to_column.keys()],
@@ -33,6 +41,28 @@ def home():
                            users=get_users(),
                            config=Config)
 
+# when the route is /bestskins
+@app.route('/bestskins')
+def best_skins():
+    # show the best skins page
+    return render_template('best_skins_or_users.html',
+                           data=get_best_in_data(False),
+                           table_head_param=["Skin", "Best Times"],
+                           title="Skins ordered by number of best timed tracks",
+                           config=Config)
+
+# when the route is /leaderboard
+@app.route('/leaderboard')
+def best_users():
+    # show the leaderboard page
+    return render_template('best_skins_or_users.html',
+                           data=get_best_in_data(True),
+                           table_head_param=["Player", "Points"],
+                           title="Players' leaderboard (following mario kart's scoring system)",
+                           config=Config)
+
+
+# run
 if __name__ == '__main__':
     app.jinja_env.auto_reload = True
     app.config['TEMPLATES_AUTO_RELOAD'] = True
