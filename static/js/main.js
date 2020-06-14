11a28b62790d5c4c47a5293f1635ed1b6dceb575
Jan-Jaap Korpershoek <jjkorpershoek96@gmail.com>
2020-06-14 21:13:15 +0200

    Fix dropdown on mobile

--- static/js/main.js
+++ static/js/main.js
@@ -1,45 +1,4 @@
-var api_url = window.location.protocol + "//" + window.location.host + '/highscores/api'
-
-var form = document.getElementById("highscores_form");
-form.addEventListener("submit", function (event) {
-    event.preventDefault()
-    submit_form()
-})
-
-location.search
-    .substr(1)
-    .split("&")
-    .forEach(item => {
-        if (item) {
-            item = item.split("=");
-            document.getElementById(item[0]).value = item[1]
-        }
-    });
-
-var submit_form = function() {
-    var url = new URL(api_url + '/search')
-    for (const pair of new FormData(form)) {
-        if (pair[1] != "") {
-            url.searchParams.append(pair[0],pair[1])
-        }
-    }
-    fetch(url)
-        .then(response => {
-            return response.json()
-        })
-        .then(data => {
-            var highscores = document.getElementById("highscores_table")
-            highscores.innerHTML = ''
-            for (const row of data) {
-                var score = document.createElement("tr")
-                for (const column of ['username', 'mapname', 'skin', 'time_string', 'datetime']) {
-                    var td = document.createElement("td")
-                    td.textContent = row[column]
-                    score.appendChild(td)
-                }
-                highscores.appendChild(score)
-            }
-        })
+function toggle_menu(event) {
+    var items = document.getElementById("navbar-items");
+    items.classList.toggle("show");
 }
-
-submit_form()
