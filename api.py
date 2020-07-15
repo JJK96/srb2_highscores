f68423c9b2efbe15c66aea4e00ccdedd13fbde5e
Jan-Jaap Korpershoek <jjkorpershoek96@gmail.com>
2020-07-15 20:25:15 +0200

    Reimplement get_best_in_data for speed
    For the leaderboard players can now be counted multiple times per map if the scores are with different skins.
    Separate search into a separate function

--- api.py
+++ api.py
@@ -7,6 +7,7 @@ from srb2_query import SRB2Query
 from config import Config
 from fuzzywuzzy import process, fuzz
 from werkzeug.exceptions import HTTPException
+from sqlalchemy import func
 
 # setup the api section of the site
 api_prefix = "/highscores/api"
@@ -98,43 +99,6 @@ def get_map_highscores():
         maps[row.map_id] = map
     return [x for x in maps.values()]
 
-# get all the highscores
-def get_highscores():
-    # get all the highscores from the database ordered by time
-    query = db.session.query(
-        Highscore.username,
-        Map.name.label("mapname"),
-        Map.id.label("map_id"),
-        Highscore.skin,
-        Highscore.time,
-        Highscore.time_string,
-        Highscore.datetime) \
-        .filter(Map.id == Highscore.map_id) \
-        .order_by(Highscore.time.asc())
-        
-    return query.all()
-
-# get all the highscores divided by map
-def get_highscores_by_map():
-    # get all the highscores
-    highscores = get_highscores()
-    # get all the maps
-    maps = get_maps()
-    # setup the results' dictionary
-    res = {}
-    # for every map
-    for map in maps:
-        temp = []
-        # for every score
-        for score in highscores:
-            # if the score's map is the same as the current map
-            if score[1] == map.name:
-                # append the score to the map's list
-                temp.append(score)
-        # append the map's list by the map's name
-        res[map.name] = temp
-    return res
-
 # Gets the best skins or leaderboard of users in the database 
 # returns the leaderboard when arg for_leaderboard is true and the best skins when it's false
 def get_best_in_data(for_leaderboard, all_skins=False):
@@ -144,62 +108,31 @@ def get_best_in_data(for_leaderboard, all_skins=False):
     
     # setup the weights for the leaderboard(mariokart's scoring system)
     weights = {
-    1:15,
-    2:12,
-    3:10,
-    4:8,
-    5:7,
-    6:6,
-    7:5,
-    8:4,
-    9:3,
-    10:2,
-    11:1,
-    12:0
+        1:15,
+        2:12,
+        3:10,
+        4:8,
+        5:7,
+        6:6,
+        7:5,
+        8:4,
+        9:3,
+        10:2,
+        11:1
     }
     
-    # get the highscores divided by map
-    highscores = get_highscores_by_map()
-    
     # for every map in the highscores
-    for map_scores in highscores:
+    for map in get_maps():
+        scores = search(filters=[Highscore.map_id == map.id], limit=11, all_skins=all_skins)
         # if must return the best skins
-        if not for_leaderboard:
-            # get the scores
-            scores = highscores[map_scores]
-            
-            # if not all the skins are requested and the skin isn't vanilla
-            if not all_skins and not (scores[0][3] in base_skins):
-                # go to the next score
-                continue
-            # save the point in the dictionary by the first score's skin
-            scoring[scores[0][3]] += 1
-        else:
-            # reset counted usernames, offset and place
-            counted_usernames = []
-            offset = 0
-            place = 0
+        if for_leaderboard:
             # for every score in the map's highscores
-            for score in highscores[map_scores]:
-                # if not all the skins are requested and the skin isn't vanilla
-                if not all_skins and not (score[3] in base_skins):
-                    # go to the next score
-                    continue
-
-                place += 1
-                # if 12 scores got counted
-                if place-offset >= 12:
-                    # get the next map
-                    break
-                # if the score's username hasn't been counted yet
-                if not score[0] in counted_usernames:
-                    # add increase the points in the dictionary by the username
-                    scoring[score[0]] += weights.get(place-offset, 0)
-                    # set the username as counted
-                    counted_usernames.append(score[0])
-                else:
-                    # add offset to the score counting
-                    offset += 1
+            for place, score in enumerate(scores):
+                # add increase the points in the dictionary by the username
+                scoring[score.username] += weights.get(place+1, 0)
+        else:
+            # save the point in the dictionary by the first score's skin
+            scoring[scores[0].skin] += 1
     # sort the dictionary by most points
     for k, v in sorted(scoring.items(), key=lambda x: x[1], reverse=True):
         res[k] = v
@@ -299,9 +232,9 @@ def api_highscores():
     resp = Response(response=json.dumps(get_map_highscores(), default=lambda o: str(o)), status=200, mimetype="application/json")
     return resp
 
-# when the route is api/search
-@api_routes.route('/search')
-def search():
+def search(filters=[], ordering=None, limit=None, all_skins=False, all_scores=False):
+    if not limit:
+        limit = 1000
     # subquery to get the best time for each combination (User, Skin, Map)
     best_scores = db.session.query(db.func.min(Highscore.time).label("time"),
                              Highscore.username,
@@ -320,29 +253,44 @@ def search():
         Highscore.time,
         Highscore.time_string,
         Highscore.datetime)
-    
-    # request the params for the type of scores
-    all_scores = request.args.get("all_scores")
-    
-    # if the request for all scores is false
-    if all_scores != "on":
+
+    if not all_scores:
         query = query.select_from(Map, db.join(Highscore, best_scores,
                                   (Highscore.username == best_scores.c.username) & \
                                   (Highscore.skin == best_scores.c.skin) & \
                                   (Highscore.map_id == best_scores.c.map_id) & \
                                   (Highscore.time == best_scores.c.time)))
-    
-    # request the params for the skins filtering the scores
-    all_skins = request.args.get("all_skins")
-    
-    if all_skins != "on":
+
+    if not all_skins:
         query = query.filter(Highscore.skin.in_(base_skins))
-                                      
+
     query = query.filter(Map.id == Highscore.map_id)
+
+    if ordering:
+        query = query.order_by(ordering)
+
+    for filter in filters:
+        query = query.filter(filter)
+
+    query = query.order_by(Highscore.time.asc())
+
+    query = query.limit(limit)
+
+    return query.all()
     
+
+# when the route is api/search
+@api_routes.route('/search')
+def api_search():
+    # request the params for the type of scores
+    all_scores = request.args.get("all_scores") == "on"
+    # request the params for the skins filtering the scores
+    all_skins = request.args.get("all_skins") == "on"
     # request the params for the ordering
     order = request.args.get('order')
     descending = request.args.get('descending')
+
+    ordering = None
     # if the order param is valid
     if order in key_to_column:
         # order by the order parameter
@@ -351,9 +299,9 @@ def search():
         if descending:
             # order in descending order
             order_by = order_by.desc()
-        query = query.order_by(order_by)
-    query = query.order_by(Highscore.time.asc())
+        ordering = order_by
 
+    filters=[]
     # for every given parameter
     for key in request.args:
         # if the parameter's key is in the highscores columns
@@ -362,18 +310,20 @@ def search():
             # if the column has to be searched through fuzzywuzzy
             try:
                 extracted = process.extractOne(request.args.get(key), fuzzy_columns[key](), scorer=fuzz.ratio)
-                query = query.filter(key_to_column[key] == extracted[0])
+                filters.append(key_to_column[key] == extracted[0])
             except KeyError:
                 # filter the highscores by such column
-                query = query.filter(key_to_column[key] == request.args.get(key))
+                filters.append(key_to_column[key] == request.args.get(key))
 
-    try:
-        query = query.limit(int(request.args.get('limit',1000)))
-    except (ValueError, TypeError):
-        return jsonify(error="Invalid limit"), 400
-        
+    limit = request.args.get('limit',None)
+    if limit:
+        try:
+            limit = int(limit)
+        except (ValueError, TypeError):
+            return jsonify(error="Invalid limit"), 400
+
+    scores = search(all_scores=all_scores, all_skins=all_skins, filters=filters, ordering=ordering, limit=limit)
     # return the query as json
-    scores = query.all()
     resp = Response(response=to_json(scores), status=200, mimetype="application/json")
     return resp
 
