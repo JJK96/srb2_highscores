e0d0e45983bf2b7ff1d17e2a302c729e9fe69ef5
Jan-Jaap Korpershoek <jjkorpershoek96@gmail.com>
2024-01-17 21:16:39 +0100

    Add speedrun server

--- client/src/routes/Github.svelte
+++ client/src/routes/Github.svelte
@@ -11,7 +11,7 @@
     <p>Feel free to make a pull request for any of these projects</p>
 
     <ul>
-        <li><a href="https://github.com/Meziu/SRB2">SRB2 server</a></li>
+        <li><a href="https://github.com/Meziu/SRB2/tree/speedrun_server">SRB2 server</a></li>
         <li><a href="https://github.com/jjk96/srb2_highscores">Website</a></li>
         <li><a href="https://github.com/Meziu/srb2_discordbot">Discord bot</a></li>
         <li><a href="https://github.com/Meziu/SRB2/tree/master/src/luascripts">Lua scripts</a></li>
